293357c5d164c2507372327b49740136e1452a64
zzin742 <josehtl07@gmail.com>
2025-07-21 16:16:09 -0300

    primeiro commit com arquivos existentes

---
+++ script.js
@@ -0,0 +1,38 @@
+
+AOS.init(
+  {
+      duration: 1200,
+  }
+);
+
+var swiper = new Swiper(".mySwiper", {
+  grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        translate: ["-120%", 0, -500],
+      },
+      next: {
+        shadow: true,
+        translate: ["120%", 0, -500],
+      },
+    },
+});
+
+const hamburger = document.querySelector(".hamburger")
+const navContent = document.querySelector(".nav-content")
+const body = document.body;
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active")
+  navContent.classList.toggle("active")
+  body.classList.toggle("no-scroll");
+})
+
+function closeMenu() {
+  hamburger.classList.remove("active")
+  navContent.classList.remove("active")
+  body.classList.remove("no-scroll");
+}
+
